6a1d48fcebf915d33379ae1104f2e57225079543
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-18 12:35:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 class Person {
-    constructor(name, age) {
-        this.Name = name;
-        this.Age = age;
-    }
-get getName(){ return this.Name}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-    set setage(age) {
-     //   console.log(`The age is ${newage}`);
-		this.Age = age;
-    }
-	
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for age
+  set age(value) {
+    this._age = value;
+  }
 }
 
 class Student extends Person {
-    constructor(name, age) {
-        super(name, age);
-    }
+  // Constructor for Student class
+  constructor(name, age) {
+    super(name, age);
+  }
 
-    study() {
-        console.log(`${this.Name} is studying`);
-    }
+  // Method specific to Student class
+  study() {
+    console.log(${this.name} is studying);
+  }
 }
 
 class Teacher extends Person {
-    teach() {
-        console.log(`${this.Name} is teaching`);
-    }
+  // Constructor for Teacher class
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  // Method specific to Teacher class
+  teach() {
+    console.log(${this.name} is teaching);
+  }
 }
 
-// Do not change the code below this line
+// Assigning classes to the window object
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
-
-
