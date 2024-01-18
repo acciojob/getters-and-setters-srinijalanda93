0134fdb2419e116dd229e384be2670ffcbf73320
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-18 11:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 class Person {
-	constructor(name,age){
-        name=this.name
-        age=this.age
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-    get getName(){
-        console.log(`the name is ${this.name}`)
+
+    get getName() {
+        console.log(`The name is ${this.name}`);
     }
-    set setName(newName){
-        console.log(`the name is ${newName.toLowerCase}`)
+
+    set setName(newName) {
+        console.log(`The name is ${newName.toLowerCase()}`);
     }
 }
 
 class Student extends Person {
-    constructor(){
-        super(name,age)
-
+    constructor(name, age) {
+        super(name, age);
     }
-    study(){
-       // that logs <name> is studying to the console.
-       console.log(`${this.name} is studying`)
+
+    study() {
+        console.log(`${this.name} is studying`);
     }
 }
 
 class Teacher extends Person {
-    teach(){
-        console.log(`${this.name} is studying`)
+    teach() {
+        console.log(`${this.name} is teaching`);
     }
 }
 
