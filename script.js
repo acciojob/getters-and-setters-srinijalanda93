0589999d4b603c5ba6c8bafd64761cd9113f58d2
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-18 11:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-//complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+        name=this.name
+        age=this.age
+    }
+    get getName(){
+        console.log(`the name is ${this.name}`)
+    }
+    set setName(newName){
+        console.log(`the name is ${newName.toLowerCase}`)
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    constructor(name,age){
+        super(name,age)
 
-class Teacher extends Person {}
+    }
+    study(){
+       // that logs <name> is studying to the console.
+       console.log(`${this.name} is studying`)
+    }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+class Teacher extends Person {
+    teach(){
+        console.log(`${this.name} is studying`)
+    }
+}
